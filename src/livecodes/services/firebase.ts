d02441efb9310f467a7bd8f276020ed8fe26941b
Thomas Scheibitz <thomas.scheibitz@in2code.de>
2024-03-01 16:59:23 +0100

    [TASK] add firebase settings

--- src/livecodes/services/firebase.ts
+++ src/livecodes/services/firebase.ts
@@ -4,11 +4,11 @@ export type { User as FirebaseUser } from 'firebase/auth';
 export { getAuth, signInWithPopup, signOut, GithubAuthProvider } from 'firebase/auth';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyB352dJ_NKCZ43G5kv9Lt-sb5nMXTJRONQ',
-  authDomain: 'livecodes-io.firebaseapp.com',
-  projectId: 'livecodes-io',
-  storageBucket: 'livecodes-io.appspot.com',
-  messagingSenderId: '756660932772',
-  appId: '1:756660932772:web:4cbbf58f809d81c2189631',
-  measurementId: 'G-C5WYBVEKF7',
+  apiKey: 'AIzaSyC9JqfYZI1vsBR3WeOw5J39p5d5f2yh-Uk',
+  authDomain: 'codescheibitzcom.firebaseapp.com',
+  projectId: 'codescheibitzcom',
+  storageBucket: 'codescheibitzcom.appspot.com',
+  messagingSenderId: '27872266784',
+  appId: '1:27872266784:web:3fb859f7318558b54a4a45',
+  measurementId: '',
 };
